b10403ebe61d9630e57801a46c3ea2bc212c7334
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2021-12-05 20:00:41 +0300

    hw3

--- create.sh
+++ create.sh
@@ -1,14 +1,6 @@
 sudo mysql -e '
-create database if not exists stud;
-use stud;
-create user if not exists "stud" identified by "stud";
-grant all on stud.* to "stud";
-CREATE TABLE IF NOT EXISTS `Person`
-(
-    `login`  VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `first_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `last_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `age` INTEGER NOT NULL,PRIMARY KEY (`login`),
-    INDEX `login` (`login`)
-);
+create database if not exists sql_test;
+use sql_test;
+create user if not exists "test" identified by "pzjqUkMnc7vfNHET";
+grant all on sql_test.* to "test";
 '
--- database/database.cpp
+++ database/database.cpp
@@ -5,6 +5,8 @@ namespace database{
     Database::Database(){
         _connection_string+="host=";
         _connection_string+=Config::get().get_host();
+        _connection_string+=";port=";
+        _connection_string+=Config::get().get_port();
         _connection_string+=";user=";
         _connection_string+=Config::get().get_login();
         _connection_string+=";db=";
@@ -14,6 +16,31 @@ namespace database{
 
         Poco::Data::MySQL::Connector::registerConnector();
     }
+
+    size_t Database::get_max_shard(){
+        return 3;
+    }
+
+    std::vector<std::string> Database::get_all_hints(){
+        std::vector<std::string> result;
+        for(size_t i=0;i<get_max_shard();++i){
+            std::string shard_name = "-- sharding:";
+            shard_name += std::to_string(i);
+            result.push_back(shard_name);
+        }
+        return result;
+    }
+
+    std::string Database::sharding_hint(std::string& login){
+
+        size_t shard_number = std::hash<std::string>{}(login)%get_max_shard();
+
+        std::string result = "-- sharding:";
+        result += std::to_string(shard_number);
+        std::cout << result << std::endl;
+        return result;
+    }
+
 
     Database& Database::get(){
         static Database _instance;
